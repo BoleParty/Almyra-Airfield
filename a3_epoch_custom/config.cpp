54ea1b67da7054d4cfca6a87e8702db11d0e54ee
BoleParty <info@tacongamingmilitia.com>
2015-09-03 13:43:54 +0200

    Create config.cpp

---
+++ a3_epoch_custom/config.cpp
@@ -0,0 +1,18 @@
+class CfgPatches {
+	class a3_epoch_custom {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"A3_server_settings"};
+	};
+};
+class CfgFunctions {
+	class Enigma {
+		class main {
+			file = "x\addons\a3_epoch_custom\init";
+			class init {
+				postInit = 1;
+			};
+		};
+	};
+};
